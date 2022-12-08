7e799d42572d1bb3cfd06e69cb40ea43a7da8ca3
Lisa Meyer <lisamermey@gmail.com>
2022-12-08 13:25:19 -0500

    implement logic for awarding points

--- day02/day02.js
+++ day02/day02.js
@@ -15,5 +15,77 @@ const getGameStrategies = async () => {
 }
 
 const playGameStrategies = (gameStrategies) => {
+  // keep score record
+  const points = {
+    you: 0,
+    opponent: 0
+  };
 
+  // points awarded to each shape
+  const shapePoints = {
+    rock: 1,
+    paper: 2,
+    scissors: 3,
+  };
+
+  // points awarded for win, lose, draw
+  const outcomePoints = {
+    win: 6,
+    lose: 0,
+    draw: 3,
+  };
+
+  for (const game of gameStrategies) {
+    switch (game[0]) {
+      case 'A':
+        points.opponent += shapePoints.rock;
+        if (game[1] === 'X') {
+          points.opponent += outcomePoints.draw;
+          points.you += outcomePoints.draw + shapePoints.rock;
+        }
+        if (game[1] === 'Y') {
+          points.opponent += outcomePoints.lose;
+          points.you += outcomePoints.win + shapePoints.paper;
+        }
+        if (game[1] === 'Z') {
+          points.opponent += outcomePoints.win;
+          points.you += outcomePoints.lose + shapePoints.scissors;
+        }
+        break;
+      case 'B':
+        points.opponent += shapePoints.paper;
+        if (game[1] === 'X') {
+          points.opponent += outcomePoints.win;
+          points.you += outcomePoints.lose + shapePoints.rock;
+        }
+        if (game[1] === 'Y') {
+          points.opponent += outcomePoints.draw;
+          points.you += outcomePoints.draw + shapePoints.paper;
+        }
+        if (game[1] === 'Z') {
+          points.opponent += outcomePoints.lose;
+          points.you += outcomePoints.win + shapePoints.scissors;
+        }
+        break;
+      case 'C':
+        points.opponent += shapePoints.paper;
+        if (game[1] === 'X') {
+          points.opponent += outcomePoints.lose;
+          points.you += outcomePoints.win + shapePoints.rock;
+        }
+        if (game[1] === 'Y') {
+          points.opponent += outcomePoints.win;
+          points.you += outcomePoints.lose + shapePoints.paper;
+        }
+        if (game[1] === 'Z') {
+          points.opponent += outcomePoints.draw;
+          points.you += outcomePoints.draw + shapePoints.scissors;
+        }
+        break;
+    }
+  }
+  // print final scores
+  console.log('FINAL SCORES:', points);
 }
+
+getGameStrategies();
